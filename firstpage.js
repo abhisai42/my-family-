9b21a5a6592902d7983d86cdb02e88e2463c5823
SAIKUMAR SAI gandesri <164223252+abhisai42@users.noreply.github.com>
2024-05-12 17:33:59 +0530

    Update firstpage.js

--- firstpage.js
+++ firstpage.js
@@ -1,9 +1,22 @@
  // JavaScript
 
-  function toggleDropdown() {
-    const dropdownContent = document.getElementById("myDropdown");
-    dropdownContent.classList.toggle("show");
-}
+ function toggleMenu() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    dropdownMenu.classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.menu-btn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
     // Define the speak function
 function speak(text) {
